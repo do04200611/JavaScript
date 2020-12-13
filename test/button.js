d31321cf165eadd9c359c807f6b4224db9cc6004
do04200611 <11naeri2@naver.com>
2020-12-13 19:08:42 +0900

    시간을 나타내는 코드입니다.
    시간을 나타내는 코드입니다.

--- test/button.js
+++
@@ -1,5 +0,0 @@
-function touchNumberButton(){
-    $("#phoneNumber").val($("#phoneNumber").val()+this.value);
-}$(document).ready(function(){
-$(".btnNubmer").click(touchNumberButton);
-});
--- test/homework.js
+++
@@ -1,6 +0,0 @@
-
-function touchNumberButton(){
-    $("#phoneNumber").val($("#phoneNumber").val()+this.value);
-}$(document).ready(function(){
-$("#btnNubmer").click(touchNumberButton);
-});
